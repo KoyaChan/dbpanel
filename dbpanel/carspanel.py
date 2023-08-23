bd0659d09370c3726f67f07dcd25748792c9ee2a
KoyaChan <KoyaChan@users.noreply.github.com>
2023-08-23 17:11:18 +0900

    Disable id field in all tabs. Assign an id when adding a car

--- dbpanel/carspanel.py
+++ dbpanel/carspanel.py
@@ -118,6 +118,7 @@ class ListTab():
 # Add the data in the fields to the database when the Add button is clicked.
 class AddTab:
     def __init__(self, panel):
+        self.panel = panel
         self.tab = panel.make_tab('Add a car')
         # Make a dictionary with pairs of attribute and StringVar.
         # It is used to contain the input data.
@@ -126,17 +127,36 @@ class AddTab:
         # Arrange the input fields in the tab page.
         panel.make_input_frame(self.tab, self.car_data_fields)
 
-        self.panel = panel
         submit_button = tk.Button(self.tab, text='Add',
                                   command=lambda: self.add_car()
                                   )
         # Place the button at the bottom of the tab page.
         submit_button.pack(anchor=tk.S)
 
+    # Return new id which is assigned to new data to be added.
+    def new_id(self):
+        # Retrieve all data from the database
+        cars = self.panel.db.get_cars_list()
+
+        # Find the smallest id which isn't assigned to any rows.
+        ids = (car['id'] for car in cars)
+        num = 1
+        for id in ids:
+            # rows are sorted by id
+            if id != num:
+                # This num isn't assigned to a row
+                return str(num)
+            num += 1
+
+        # All ids less than num has been assigned to a row
+        return str(num)
+
     # Insert the data which is entered in the Add tab to database
     def add_car(self):
         # Convert StringVar to string in the input fields
         car_data = self.panel.field_data_to_dict(self.car_data_fields)
+        car_data['id'] = self.new_id()
+        print(car_data)
 
         # Give the data to the database function to add the data
         self.panel.submit_request(car_data,
@@ -252,8 +272,10 @@ class CarsPanel:
             label = tk.Label(input_frame, text=attr)
             label.grid(row=row_num, column=1, sticky=tk.W)
             # Show the value when a field has value
+            # Prevent id field from being edited
             entry = tk.Entry(input_frame,
-                             textvariable=car_data_fields[attr]
+                             textvariable=car_data_fields[attr],
+                             state='disabled' if attr == 'id' else 'normal'
                              )
             entry.grid(row=row_num, column=2)
 
